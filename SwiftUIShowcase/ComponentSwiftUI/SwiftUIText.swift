2c94f97b8f36ad30d1b5216ca4e16bb026a8b7ea
Khamin Gleb <nimik19@yandex.ru>
2024-12-24 16:57:54 +0300

    fix (CG-03): Fix Code formatting and spacing

--- SwiftUIShowcase/ComponentSwiftUI/SwiftUIText.swift
+++ SwiftUIShowcase/ComponentSwiftUI/SwiftUIText.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct SwiftUIText: View {
-    
+
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -33,9 +33,9 @@ struct SwiftUIText: View {
                     .font(.caption)
                 Text("Hellow, World")
                     .font(.caption2)
-                
+
                 TextTitleVIew(title: "fontWeight")
-                
+
                 Text("Hellow, World")
                     .fontWeight(.black)
                 Text("Hellow, World")
@@ -54,16 +54,16 @@ struct SwiftUIText: View {
                     .fontWeight(.thin)
                 Text("Hellow, World")
                     .fontWeight(.ultraLight)
-                
+
                 TextTitleVIew(title: "style")
-                
+
                 Text("Hellow, World")
                     .bold()
                 Text("Hellow, World")
                     .italic()
-                
+
                 TextTitleVIew(title: "underline")
-                
+
                 Text("Hellow, World")
                     .underline()
                 Text("Hellow, World")
@@ -74,9 +74,9 @@ struct SwiftUIText: View {
                     .underline(false, color: .red)
                 Text("Hellow, World")
                     .underline(true, color: .red)
-                
+
                 TextTitleVIew(title: "strikethrough")
-                
+
                 Text("Hellow, World")
                     .strikethrough()
                 Text("Hellow, World")
@@ -87,9 +87,9 @@ struct SwiftUIText: View {
                     .strikethrough(false, color: .red)
                 Text("Hellow, World")
                     .strikethrough(true, color: .red)
-                
+
                 TextTitleVIew(title: "fontDesign")
-                
+
                 Text("Hellow, World")
                     .fontDesign(.default)
                 Text("Hellow, World")
@@ -100,23 +100,23 @@ struct SwiftUIText: View {
                     .fontDesign(.monospaced)
                 Text("Hellow, World")
                     .fontDesign(.serif)
-                
+
                 TextTitleVIew(title: "foregroundStyle")
-                
+
                 Text("Hellow, World")
                     .foregroundStyle(Color.blue)
                 Text("Hellow, World")
                     .foregroundStyle(Color.red.opacity(0.3))
-                
+
                 TextTitleVIew(title: "font.system")
-                
+
                 Text("Hellow, World")
                     .font(.system(size: 40))
                 Text("Hellow, World")
                     .font(.system(size: 40, weight: .ultraLight, design: .default))
-                
+
                 TextTitleVIew(title: "multilineTextAlignment")
-                
+
                 VStack(spacing: 10) {
                     Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
                     Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
@@ -126,9 +126,9 @@ struct SwiftUIText: View {
                     Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
                         .multilineTextAlignment(.center)
                 }
-                
+
                 TextTitleVIew(title: "baselineOffset")
-                
+
                 Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
                 Divider()
                 Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
@@ -140,9 +140,9 @@ struct SwiftUIText: View {
                 Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
                     .multilineTextAlignment(.center)
                 Divider()
-                
+
                 TextTitleVIew(title: "kerning")
-                
+
                 Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
                 Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
                     .kerning(0.0)
@@ -150,9 +150,9 @@ struct SwiftUIText: View {
                     .kerning(2.0)
                 Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
                     .kerning(-2.0)
-                
+
                 TextTitleVIew(title: "minimumScaleFactor")
-                
+
                 Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
                     .frame(width: 200, height: 100, alignment: .center)
                 Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
@@ -161,7 +161,7 @@ struct SwiftUIText: View {
                 Text("Writing code in Swift is a true pleasure! This programming language combines simplicity and power, allowing developers to create efficient and safe applications.")
                     .frame(width: 300, height: 150, alignment: .center)
                     .minimumScaleFactor(0.1)
-                
+
                 TextTitleVIew(title: "uppercased/lowercased/capitalized")
                 
                 Text("Writing code in Swift is a true pleasure!".uppercased())
